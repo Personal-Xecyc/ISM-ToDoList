609a82d4dbd4e66f6921d05921ef9e79b5bf4230
Dima <kb201_kdo@student.ztu.edu.ua>
2021-02-10 00:20:56 +0200

    release v1

---
+++ ToDoList_ISM/Models/TodoList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoList_ISM.Models
+{
+    public class TodoList
+    {
+        public int iD { get; set; }
+        [Required]
+        public string Content { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
